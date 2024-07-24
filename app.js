17c2963f282517e3fa4f807a6c880f209f24a0ec
KOSmek <154541438+KOSmekKosmik@users.noreply.github.com>
2024-07-24 14:36:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-var string;
-var myButton = document.getElementById("my");
 
-string = prompt("hej","hello");
-function alertButton() {
- myButton.textContent = string
- 
-}
-function resetbutton(){
-    string = prompt("hej","hello");
-}
